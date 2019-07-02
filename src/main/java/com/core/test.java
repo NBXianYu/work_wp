e4f15893a406d6b4b0ad758d925cdc675f91a8a7
TheoWu <13668377258@sina.cn>
2019-07-02 09:30:43 +0800

    redis

---
+++ src/main/java/com/core/test.java
@@ -0,0 +1,16 @@
+package com.core;
+
+/**
+ * @Author 吴鹏
+ * @Description:
+ */
+public class test {
+
+    public static void main(String arg[]) {
+
+        float f = 1.1f;
+        System.out.println(f);
+
+    }
+
+}
